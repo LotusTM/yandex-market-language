2769c519cd39f3d26da108d284f065dda37b74ed
ArmorDarks <git@lavrins.com>
2017-09-07 19:31:44 +0300

    [tests][utils] add tcomb additional refinements

---
+++ tests/utils/tcomb.js
@@ -0,0 +1,17 @@
+const t = require('tcomb')
+
+module.exports = {
+  Absoluteurl: t.refinement(t.String, (u) => /^\/\/|:\/\//.test(u), 'Absolute url'),
+  Imagepath: t.refinement(t.String, (i) => /.(jpg|jpeg|gif|png|svg)$/.test(i), 'Image file'),
+
+  /**
+   * Ensures that value does not exceed specified length
+   * @param  {number}     max  Max or equal length of value
+   * @param  {function}   type tcomb Type to be refined. Works with
+   *                      anything that have `length` property
+   * @return {*} Passed in value
+   * @example
+   *  Maxlength(12)(t.String)('Hey, this is a long string!')
+   */
+  Maxlength: (max) => (type) => t.refinement(t.Number(max) && t.Type(type), (t) => t.length <= max, `Maxlength ${max}`),
+}
---
+++ tests/utils/tcomb.test.js
@@ -0,0 +1,75 @@
+/* eslint-env jest */
+
+const t = require('tcomb')
+const r = require('./tcomb')
+
+describe('Tcomb refinement', () => {
+  describe('Absoluteurl', () => {
+    it('should pass absolute url', () => {
+      expect(r.Absoluteurl('http://test.com')).toMatchSnapshot()
+      expect(r.Absoluteurl('https://test.com')).toMatchSnapshot()
+      expect(r.Absoluteurl('//test.com')).toMatchSnapshot()
+    })
+    it('should error on everything except absolute url', () => {
+      expect(() => r.Absoluteurl(true)).toThrowErrorMatchingSnapshot()
+      expect(() => r.Absoluteurl('/url')).toThrowErrorMatchingSnapshot()
+      expect(() => r.Absoluteurl('url/')).toThrowErrorMatchingSnapshot()
+      expect(() => r.Absoluteurl('url')).toThrowErrorMatchingSnapshot()
+      expect(() => r.Absoluteurl(-1)).toThrowErrorMatchingSnapshot()
+      expect(() => r.Absoluteurl(1)).toThrowErrorMatchingSnapshot()
+      expect(() => r.Absoluteurl(-1.555)).toThrowErrorMatchingSnapshot()
+      expect(() => r.Absoluteurl(1.555)).toThrowErrorMatchingSnapshot()
+      expect(() => r.Absoluteurl(0)).toThrowErrorMatchingSnapshot()
+      expect(() => r.Absoluteurl(null)).toThrowErrorMatchingSnapshot()
+      expect(() => r.Absoluteurl(undefined)).toThrowErrorMatchingSnapshot()
+    })
+  })
+
+  describe('Imagepath', () => {
+    it('should pass path with image file', () => {
+      expect(r.Imagepath('file.jpg')).toMatchSnapshot()
+      expect(r.Imagepath('file.jpeg')).toMatchSnapshot()
+      expect(r.Imagepath('file.png')).toMatchSnapshot()
+      expect(r.Imagepath('file.gif')).toMatchSnapshot()
+      expect(r.Imagepath('file.svg')).toMatchSnapshot()
+    })
+    it('should error on everything except path with image file', () => {
+      expect(() => r.Imagepath(true)).toThrowErrorMatchingSnapshot()
+      expect(() => r.Imagepath('http://test.com/imagelikepath.svg/')).toThrowErrorMatchingSnapshot()
+      expect(() => r.Imagepath('http://test.com')).toThrowErrorMatchingSnapshot()
+      expect(() => r.Imagepath('/url')).toThrowErrorMatchingSnapshot()
+      expect(() => r.Imagepath('url/')).toThrowErrorMatchingSnapshot()
+      expect(() => r.Imagepath('url')).toThrowErrorMatchingSnapshot()
+      expect(() => r.Imagepath(-1)).toThrowErrorMatchingSnapshot()
+      expect(() => r.Imagepath(1)).toThrowErrorMatchingSnapshot()
+      expect(() => r.Imagepath(-1.555)).toThrowErrorMatchingSnapshot()
+      expect(() => r.Imagepath(1.555)).toThrowErrorMatchingSnapshot()
+      expect(() => r.Imagepath(0)).toThrowErrorMatchingSnapshot()
+      expect(() => r.Imagepath(null)).toThrowErrorMatchingSnapshot()
+      expect(() => r.Imagepath(undefined)).toThrowErrorMatchingSnapshot()
+    })
+  })
+
+  describe('Maxlength', () => {
+    it('should pass string with less or equal length', () => {
+      expect(r.Maxlength(3)(t.String)('12')).toMatchSnapshot()
+      expect(r.Maxlength(3)(t.String)('123')).toMatchSnapshot()
+    })
+    it('should pass array with less or equal length', () => {
+      expect(r.Maxlength(3)(t.list(t.Any))([1, 2])).toMatchSnapshot()
+      expect(r.Maxlength(3)(t.list(t.Any))([1, 2, 3])).toMatchSnapshot()
+    })
+    it('should error on not tcomb type', () => {
+      expect(() => r.Maxlength(3)('nope')('12345678')).toThrowErrorMatchingSnapshot()
+    })
+    it('should error on not `number` max', () => {
+      expect(() => r.Maxlength('nope')(t.String)('12345678')).toThrowErrorMatchingSnapshot()
+    })
+    it('should error on long strings', () => {
+      expect(() => r.Maxlength(3)(t.String)('1234')).toThrowErrorMatchingSnapshot()
+    })
+    it('should error on long arrays', () => {
+      expect(() => r.Maxlength(3)(t.list(t.Any))([1, 2, 3, 4])).toThrowErrorMatchingSnapshot()
+    })
+  })
+})
