d28c98a2731780c7b92d775dee981d1c046b8ee0
ArmorDarks <git@lavrins.com>
2017-09-07 23:25:47 +0300

    [lib][tests] add utils and their tests

---
+++ tests/utils.test.js
@@ -0,0 +1,50 @@
+/* eslint-env jest */
+
+const builder = require('xmlbuilder')
+const {
+  forNode,
+  forNodeEach,
+  cleanse
+} = require('../lib/utils')
+
+describe('forNode function', () => {
+  it('should invoke callback for each element in array for node', () => {
+    const xml = builder.create('root')
+
+    forNode(xml, { one: 'oneTest', two: 2 }, (parent, prop, value) => {
+      parent.node(prop, {}, value)
+    })
+
+    expect(xml.end({ pretty: true })).toMatchSnapshot()
+  })
+
+  it('should error on non-object', () => {
+    expect(() => forNode(builder.create('root'), ['nope'], () => {})).toThrowErrorMatchingSnapshot()
+    expect(() => forNode(builder.create('root'), null, () => {})).toThrowErrorMatchingSnapshot()
+  })
+})
+
+describe('forNodeEach function', () => {
+  it('should invoke callback for each element in array for node', () => {
+    const xml = builder.create('root')
+
+    forNodeEach(xml, ['one', 'two'], (parent, value, index) => {
+      parent.node(index, {}, value)
+    })
+
+    expect(xml.end({ pretty: true })).toMatchSnapshot()
+  })
+
+  it('should error on non-array', () => {
+    expect(() => forNodeEach(builder.create('root'), { nope: 'nope' }, () => {})).toThrowErrorMatchingSnapshot()
+    expect(() => forNodeEach(builder.create('root'), null, () => {})).toThrowErrorMatchingSnapshot()
+  })
+})
+
+describe('cleanse function', () => {
+  it('should cleanse object', () => {
+    expect(cleanse({ one: 'test', undefined: undefined })).toMatchSnapshot()
+    expect(cleanse({ one: 'test', null: null })).toMatchSnapshot()
+    expect(cleanse({ one: 'test', name: 'Ted', age: 100 }, 'name', 'age')).toMatchSnapshot()
+  })
+})
