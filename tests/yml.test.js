a23fd5a568709b1094260e0676abc7962e12bfe4
ArmorDarks <git@lavrins.com>
2017-09-12 21:35:20 +0300

    [tests] in `create` test do not end to ensure that it returns valid XMLElement

--- tests/yml.test.js
+++ tests/yml.test.js
@@ -4,11 +4,7 @@ const yml = require('../lib/yml')
 
 describe('yml function', () => {
   it('`create` method should create YML', () => {
-    expect(yml({
-      name: 'BestSeller',
-      company: 'Tne Best inc.',
-      url: 'http://best.seller.ru'
-    }, { validate: false, date: '2017-09-07' }).create().end({ pretty: true })).toMatchSnapshot()
+    expect(yml({ name: 'BestSeller' }, { validate: false, date: '2017-09-07' }).create()).toMatchSnapshot()
   })
 
   it('`end` method should create and immidiately print YML', () => {
